ea716a525b5ef8158f1c0a488ffacc45aed566e2
TofikSaliba <tofik.saliba@gmail.com>
2022-05-02 22:43:59 +0300

    reset button and reusuable code

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ function startGame() {
     splash.style.animation = "fadeOut 1.5s ease-in forwards";
     document.querySelector(".toolsBox").style.visibility = "visible";
 
-    setUnits(game);
-    // console.dir(game.units);
-    // console.dir(game.tools);
-    drawTerrain(game);
-    styleClouds(game);
-    toolBoxListener();
-    bankListener();
+    setUnits(game1);
+    drawTerrain(game1);
+    styleClouds(game1);
+    toolBoxListener(game1);
+    bankListener(game1);
+    resetButton(game1);
     setTimeout(() => {
       splash.style.display = "none";
       splash.style.zIndex = "-100";
-      startListening(game);
+      startListening(game1);
     }, 1500);
   });
 }
@@ -48,12 +47,12 @@ function setUnits({ units, ROW_NUM, COL_NUM }) {
 function drawTerrain({ units, ROW_NUM, COL_NUM }) {
   for (let i = ROW_NUM - 1; i >= 0; i--) {
     for (let j = 0; j < COL_NUM; j++) {
-      giveType(units[i][j], i);
+      giveType(units[i][j], i, units);
     }
   }
 }
 
-function giveType(unit, index) {
+function giveType(unit, index, units) {
   const rand = (Math.random() * 4) | 0;
   switch (matrix[index][rand]) {
     case 0:
@@ -70,7 +69,7 @@ function giveType(unit, index) {
         unit.yIndex % 4 === 3 &&
         unit.yIndex < 197
       ) {
-        makeTree(unit, index);
+        makeTree(unit, index, units);
       }
       break;
     case 3:
@@ -79,7 +78,7 @@ function giveType(unit, index) {
         unit.yIndex < 198 &&
         unit.yIndex > 1
       ) {
-        makeRock(unit, index);
+        makeRock(unit, index, units);
       }
       break;
     case 4:
@@ -123,18 +122,18 @@ function styleClouds({ units, COL_NUM }) {
   }
 }
 
-function makeTree(unit, index) {
+function makeTree(unit, index, units) {
   unit.setAttribute("Data-type", "tree");
   for (let i = 1; i < 6; i++) {
     if (i < 3) {
-      game.units[index - i][unit.yIndex].setAttribute("Data-type", "tree");
+      units[index - i][unit.yIndex].setAttribute("Data-type", "tree");
     } else {
-      game.units[index - i][unit.yIndex - 1].setAttribute(
+      units[index - i][unit.yIndex - 1].setAttribute(
         "Data-type",
         "treeLeafs"
       );
-      game.units[index - i][unit.yIndex].setAttribute("Data-type", "treeLeafs");
-      game.units[index - i][unit.yIndex + 1].setAttribute(
+      units[index - i][unit.yIndex].setAttribute("Data-type", "treeLeafs");
+      units[index - i][unit.yIndex + 1].setAttribute(
         "Data-type",
         "treeLeafs"
       );
@@ -142,37 +141,37 @@ function makeTree(unit, index) {
   }
 }
 
-function makeRock(unit, index) {
+function makeRock(unit, index, units) {
   unit.setAttribute("Data-type", "rock");
   if (unit.yIndex > 197) {
     return;
   }
   switch ((Math.random() * 4) | 0) {
     case 0:
-      if (!game.units[index - 1][unit.yIndex].getAttribute("Data-type")) {
-        game.units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
+      if (!units[index - 1][unit.yIndex].getAttribute("Data-type")) {
+        units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
       }
-      if (!game.units[index][unit.yIndex - 1].getAttribute("Data-type")) {
-        game.units[index][unit.yIndex - 1].setAttribute("Data-type", "rock");
+      if (!units[index][unit.yIndex - 1].getAttribute("Data-type")) {
+        units[index][unit.yIndex - 1].setAttribute("Data-type", "rock");
       }
       break;
     case 1:
-      if (!game.units[index - 1][unit.yIndex].getAttribute("Data-type")) {
-        game.units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
+      if (!units[index - 1][unit.yIndex].getAttribute("Data-type")) {
+        units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
       }
-      if (!game.units[index][unit.yIndex + 1].getAttribute("Data-type")) {
-        game.units[index][unit.yIndex + 1].setAttribute("Data-type", "rock");
+      if (!units[index][unit.yIndex + 1].getAttribute("Data-type")) {
+        units[index][unit.yIndex + 1].setAttribute("Data-type", "rock");
       }
       break;
     case 2:
-      if (!game.units[index - 1][unit.yIndex].getAttribute("Data-type")) {
-        game.units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
+      if (!units[index - 1][unit.yIndex].getAttribute("Data-type")) {
+        units[index - 1][unit.yIndex].setAttribute("Data-type", "rock");
       }
-      if (!game.units[index][unit.yIndex + 1].getAttribute("Data-type")) {
-        game.units[index][unit.yIndex + 1].setAttribute("Data-type", "rock");
+      if (!units[index][unit.yIndex + 1].getAttribute("Data-type")) {
+        units[index][unit.yIndex + 1].setAttribute("Data-type", "rock");
       }
-      if (!game.units[index][unit.yIndex + 2].getAttribute("Data-type")) {
-        game.units[index][unit.yIndex + 2].setAttribute("Data-type", "rock");
+      if (!units[index][unit.yIndex + 2].getAttribute("Data-type")) {
+        units[index][unit.yIndex + 2].setAttribute("Data-type", "rock");
       }
       break;
     default:
@@ -180,7 +179,7 @@ function makeRock(unit, index) {
   }
 }
 
-function checkCollapse(x, y) {
+function checkCollapse(x, y, game) {
   let top = game.units[x - 1]
       ? game.units[x - 1][y].getAttribute("Data-type")
       : true,
@@ -223,8 +222,8 @@ function checkCollapse(x, y) {
   return false;
 }
 
-function removeBlock(x, y) {
-  if (checkCollapse(x, y)) {
+function removeBlock(x, y, game) {
+  if (checkCollapse(x, y, game)) {
     return false;
   }
   if (
@@ -244,22 +243,22 @@ function removeBlock(x, y) {
   return false;
 }
 
-function buildBlock(x, y, building) {
-  let temp = building.block;
+function buildBlock(x, y, game) {
+  let temp = game.building.block;
   if (
     temp === "tree" &&
     game.units[x + 1] &&
     game.units[x + 1][y].getAttribute("Data-type") &&
     game.units[x + 1][y].getAttribute("Data-type") === "dirtGrass"
   ) {
-    if (game.bank[building[building.block]] > 0) {
-      game.units[x][y].setAttribute("Data-type", building.block);
+    if (game.bank[game.building[game.building.block]] > 0) {
+      game.units[x][y].setAttribute("Data-type", game.building.block);
       return true;
     }
   }
-  if (building.block === "treeLeafs") {
+  if (game.building.block === "treeLeafs") {
     temp = "tree";
-  } else if (building.block === "dirtGrass") {
+  } else if (game.building.block === "dirtGrass") {
     temp = "dirt";
   }
   if (
@@ -278,21 +277,21 @@ function buildBlock(x, y, building) {
       game.units[x][y + 1].getAttribute("Data-type") &&
       game.units[x][y + 1].getAttribute("Data-type").indexOf(temp) !== -1)
   ) {
-    if (game.bank[building[building.block]] > 0) {
-      game.units[x][y].setAttribute("Data-type", building.block);
+    if (game.bank[game.building[game.building.block]] > 0) {
+      game.units[x][y].setAttribute("Data-type", game.building.block);
       return true;
     }
   }
   if (x + 1 === 20 && temp.indexOf("dirt") !== -1) {
-    if (game.bank[building[building.block]] > 0) {
-      game.units[x][y].setAttribute("Data-type", building.block);
+    if (game.bank[game.building[game.building.block]] > 0) {
+      game.units[x][y].setAttribute("Data-type", game.building.block);
       return true;
     }
   }
   return false;
 }
 
-function removeTree(x, y) {
+function removeTree(x, y, game) {
   if (!game.units[x - 1][y].getAttribute("Data-type")) {
     game.units[x][y].removeAttribute("Data-type");
     return true;
@@ -300,7 +299,7 @@ function removeTree(x, y) {
   return false;
 }
 
-function toolBoxListener() {
+function toolBoxListener(game) {
   const tera = document.querySelector("#terrain");
   const tools = document.querySelectorAll(".tools img");
   const bank = document.querySelectorAll(".bank div");
@@ -327,7 +326,7 @@ function toolBoxListener() {
   }
 }
 
-function bankListener() {
+function bankListener(game) {
   const tera = document.querySelector("#terrain");
   const tools = document.querySelectorAll(".tools img");
   const bank = document.querySelectorAll(".bank div");
@@ -350,7 +349,28 @@ function bankListener() {
   }
 }
 
-function startListening({ building }) {
+function resetButton(game) {
+  const resetBtn = document.querySelector(".crafting button");
+  const bankCounters = document.querySelectorAll(".bank span");
+  resetBtn.addEventListener("click", () => {
+    document.querySelector("#terrain").innerHTML = "";
+    while (game.units.length > 0) {
+      game.units.pop();
+    }
+    game.bank = new Array(5).fill(0);
+    bankCounters.forEach((item) => {
+      item.innerText = 0;
+      item.style.color = "red";
+    });
+    setUnits(game);
+    setTimeout(() => {
+      drawTerrain(game);
+      styleClouds(game);
+    }, 1000);
+  });
+}
+
+function startListening(game) {
   const tera = document.querySelector("#terrain");
   tera.addEventListener("click", (e) => {
     let x = e.target.xIndex,
@@ -361,7 +381,7 @@ function startListening({ building }) {
         break;
       case "dirtGrass":
         if (game.currTool === "shovel") {
-          if (removeBlock(x, y)) {
+          if (removeBlock(x, y, game)) {
             game.bank[1] += 1;
             bankCounters[1].style.color = "#fff";
             bankCounters[1].innerText = game.bank[1];
@@ -370,7 +390,7 @@ function startListening({ building }) {
         break;
       case "dirt":
         if (game.currTool === "shovel") {
-          if (removeBlock(x, y)) {
+          if (removeBlock(x, y, game)) {
             game.bank[0] += 1;
             bankCounters[0].style.color = "#fff";
             bankCounters[0].innerText = game.bank[0];
@@ -379,7 +399,7 @@ function startListening({ building }) {
         break;
       case "tree":
         if (game.currTool === "axe") {
-          if (removeTree(x, y)) {
+          if (removeTree(x, y, game)) {
             game.bank[3] += 1;
             bankCounters[3].style.color = "#fff";
             bankCounters[3].innerText = game.bank[3];
@@ -388,7 +408,7 @@ function startListening({ building }) {
         break;
       case "treeLeafs":
         if (game.currTool === "axe") {
-          if (removeBlock(x, y)) {
+          if (removeBlock(x, y, game)) {
             game.bank[4] += 1;
             bankCounters[4].style.color = "#fff";
             bankCounters[4].innerText = game.bank[4];
@@ -397,7 +417,7 @@ function startListening({ building }) {
         break;
       case "rock":
         if (game.currTool === "pickaxe") {
-          if (removeBlock(x, y)) {
+          if (removeBlock(x, y, game)) {
             game.bank[2] += 1;
             bankCounters[2].style.color = "#fff";
             bankCounters[2].innerText = game.bank[2];
@@ -405,9 +425,9 @@ function startListening({ building }) {
         }
         break;
       default:
-        let index = building[building.block];
+        let index = game.building[game.building.block];
         if (game.currTool === "build") {
-          if (buildBlock(x, y, building)) {
+          if (buildBlock(x, y, game)) {
             game.bank[index] -= 1;
             bankCounters[index].innerText = game.bank[index];
             if (game.bank[index] === 0) {
@@ -420,9 +440,8 @@ function startListening({ building }) {
   });
 }
 
-const game = {
+const game1 = {
   units: [],
-  tools: [],
   COL_NUM: 200,
   ROW_NUM: 20,
   currTool: "shovel",
